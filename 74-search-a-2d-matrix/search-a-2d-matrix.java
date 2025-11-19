608caa4dc0acdfc33e247c81ce7dc399eee69e93
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-11-19 11:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (8.27%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,18 +3,22 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int left = 0, right = m * n - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int mid_val = matrix[mid / n][mid % n];
-
-            if (mid_val == target)
-                return true;
-            else if (mid_val < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        return search(matrix,left,right,n,target);
+    }
+    public boolean search(int[][] matrix,int left,int right,int n,int target){
+        if(left>right){
+            return false;
         }
-        return false;
+        int mid=left+(right-left)/2;
+        int mid_value=matrix[mid/n][mid%n];
+        if(mid_value==target){
+            return true;
+        }
+        else if(mid_value>target){
+            return search(matrix,left,mid-1,n,target);
+        }
+        else{
+            return search(matrix,mid+1,right,n,target);
+        }
     }
 }
