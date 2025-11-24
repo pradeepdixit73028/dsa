c33651ce4129c12f1e0f2c23dd0d317ef8a22207
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-11-24 16:34:36 +0530

    Time: 154 ms (5.23%) | Memory: 42.7 MB (22.16%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -2,12 +2,20 @@ class Solution {
     public boolean predictTheWinner(int[] nums) {
         int s=0;
         int n= nums.length;
-        return helper(nums,s,n-1)>=0;
+        return helper(nums,s,0,n-1)>=0;
     }
-    public int helper(int[] nums,int i,int j){
-        if(i>=j) return nums[i];
-        int pleft= nums[i]-helper(nums,i+1,j);
-        int pright= nums[j]-helper(nums,i,j-1);
-        return Math.max(pleft,pright);
+    public int helper(int[] nums,int i,int t,int j){
+        if(i>j) return 0;
+        int ans=0;
+        if(t%2==0){
+            int pleft= nums[i]+helper(nums,i+1,t+1,j);
+            int pright= nums[j]+helper(nums,i,t+1,j-1);
+            ans= Math.max(pleft,pright);
+        }else{
+            int pleft= -nums[i]+helper(nums,i+1,t+1,j);
+            int pright= -nums[j]+helper(nums,i,t+1,j-1);
+            ans= Math.min(pleft,pright);
+        }
+        return ans;
     }
 }
