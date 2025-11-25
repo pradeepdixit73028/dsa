9c91686dc95b578e2c42b8fa3d6e6415d9dc4f6e
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-11-25 15:50:56 +0530

    Time: 9 ms (21.92%) | Memory: 102 MB (5.18%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,34 +9,15 @@
  * }
  */
 class Solution {
+    ListNode node;
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        if(head == null || head.next == null)return true;
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode newNode = reverseList(slow.next);
-        ListNode f = head;
-        ListNode s = newNode;
-        while(s != null){
-            if(f.val != s.val){
-                reverseList(newNode);
-                return false;
-            }
-            f = f.next;
-            s = s.next;
-        }
-        reverseList(newNode);
-        return true;  
-    }
-    public ListNode reverseList(ListNode head){
-        if(head == null || head.next == null) return head;
-        ListNode newHead = reverseList(head.next);
-        ListNode front = head.next;
-        front.next= head;
-        head.next = null;
-        return newHead;
+        if(head == null) return true;   
+        if(node == null) node = head;
+        boolean bool = isPalindrome(head.next);
+        if(head.val == node.val)
+            node = node.next;
+        else
+            bool = false;
+        return bool;
     }
 }
