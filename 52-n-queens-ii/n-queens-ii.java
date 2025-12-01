c83712ed9416abb351926de04b6045077e92bbb5
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-01 19:50:58 +0530

    Time: 2 ms (51.08%) | Memory: 42.1 MB (25.38%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    int count;
+    public int totalNQueens(int n) {
+       char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++){
+            Arrays.fill(board[i], '.');
+        }
+        count=0;
+        generate(board, 0);
+        return count; 
+    }
+    public void generate(char[][] board, int row) { 
+        int n = board.length;
+        if(row == n){
+            count++;
+            return;
+        } 
+        for(int j = 0; j < board[0].length ;j++) { 
+            if(isSafe(board,row,j)) { 
+                board[row][j] = 'Q'; 
+                generate(board,row+1); 
+                board[row][j] = '.'; 
+            } 
+        } 
+    } 
+    private static boolean isSafe(char[][] board, int row, int col) {
+        for(int i = 0; i< row;i++){ 
+            if(board[i][col] == 'Q')return false; 
+        }
+        int maxL= Math.min(row,col);
+        for(int j =1; j<=maxL;j++){ 
+            if(board[row-j][col-j] == 'Q')return false; 
+        } 
+        int maxR= Math.min(row,board.length-1-col);
+        for(int j =1; j<=maxR;j++){ 
+            if(board[row-j][col+j] == 'Q')return false; 
+        } 
+        return true; 
+    } 
+}
