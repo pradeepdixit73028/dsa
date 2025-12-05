d490d170531156aa9349709daca968c01d7cb3ed
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-05 20:32:40 +0530

    Time: 66 ms (13.38%) | Memory: 47.8 MB (28.73%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,23 +1,18 @@
-// -----------------------------------------------------------
-// 2. Memoization (Top-Down DP)
-// -----------------------------------------------------------
 class Solution {
-    boolean solve(int nums[], int i, int dp[]) {
-        if(i >= nums.length-1) return true;
-        if (dp[i] != -1) return dp[i] == 1;
-
-        for(int step = 1; step <= nums[i]; step++) {
-            if(solve(nums, i+step, dp)) {
-                dp[i] = 1;
-                return true;
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        boolean dp[] = new boolean[n];
+        Arrays.fill(dp, false);
+        dp[n-1] = true;
+        for(int i = n-2; i>=0; i--) {
+            int maxJump = Math.min(i+nums[i], n-1);
+            for(int j = i+1; j <= maxJump; j++) {
+                if(dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        dp[i] = 0;
-        return false;
-    }
-    public boolean canJump(int[] nums) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return solve(nums, 0, dp);
+        return dp[0];
     }
 }
