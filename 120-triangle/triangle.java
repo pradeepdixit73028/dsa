b4cde3101bd7af44a2d3a9eec90870d90e5aa508
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-08 12:12:06 +0530

    Time: 6 ms (31.18%) | Memory: 46.4 MB (12.02%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,14 +2,16 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         Integer [][]dp= new Integer[n][n];
-        return solve(0,0,n,triangle,dp);
-    }
-    public int solve(int i,int j,int n,List<List<Integer>> t,Integer [][]dp){
-        if(i==n-1) return t.get(i).get(j);
-        if(dp[i][j]!=null) return dp[i][j];
-        int down = t.get(i).get(j)+solve(i+1,j,n,t,dp);
-        int diagonal = t.get(i).get(j)+solve(i+1,j+1,n,t,dp);
-        dp[i][j] = Math.min(down,diagonal);
-        return dp[i][j]; 
+        for(int i=0;i<n;i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j)+dp[i+1][j];
+                int diagonal = triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j] = Math.min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
