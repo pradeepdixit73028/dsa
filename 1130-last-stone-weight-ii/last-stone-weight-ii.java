1d969117b1f29eb3aa85851d128e8ddd257c7129
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-09 11:58:00 +0530

    Time: 2 ms (92.93%) | Memory: 44.3 MB (18.73%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -1,18 +1,27 @@
 class Solution {
-    public int lastStoneWeightII(int[] stones) {
-        int sum = 0;
-        for(int stone : stones) sum += stone;
-        Integer[][] dp= new Integer[stones.length][sum];
-        return helper(stones, 0, 0, 0, dp);
+    private int solve(int[] arr,int n,int sum,Integer dp[][])
+    {
+        if(n==0)
+            return 0;
+        if(sum<=0)
+            return 0;
+        if(dp[n][sum]!=null)
+            return dp[n][sum];
+        if(arr[n-1]<=sum)
+            return dp[n][sum]=Math.max(arr[n-1]+solve(arr,n-1,sum-arr[n-1],dp),
+                                     solve(arr,n-1,sum,dp));
+        else
+            return dp[n][sum]=solve(arr,n-1,sum,dp);
     }
-    private int helper(int[] stones,int i,int sumL,int sumR,Integer[][] dp) {
-        if(i == stones.length){
-            return Math.abs(sumL - sumR);
-        }
-        if(dp[i][sumL] != null) {
-            return dp[i][sumL];
-        }
-        dp[i][sumL] = Math.min(helper(stones, i+1, sumL + stones[i], sumR, dp) , helper(stones, i+1, sumL, sumR + stones[i], dp));
-        return dp[i][sumL];
+    public int lastStoneWeightII(int[] stones) {
+        int n=stones.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=stones[i];
+        int half=sum/2;
+        Integer dp[][]= new Integer[n+1][half+1];
+        int p1=solve(stones,n,half,dp);
+        int p2=sum-p1;
+        return Math.abs(p1-p2);
     }
 }
