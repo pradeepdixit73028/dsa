5d81f9c564a91874cb2aa5b01e518817280ea3f0
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-09 10:47:43 +0530

    Time: 33 ms (17.33%) | Memory: 47.8 MB (5.41%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,21 +2,24 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n= coins.length;
         Integer [][]dp= new Integer[n][amount+1];
-        int ans = solve(n-1,amount,coins,dp);
+        dp[0][0]=0;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }
+            else dp[0][i]=(int)1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notpick=dp[i-1][j];
+                int pick=(int)1e9;
+                if(coins[i]<=j){
+                    pick = 1+dp[i][j-coins[i]];
+                }
+                dp[i][j]=Math.min(pick,notpick);
+            }
+        }
+        int ans = dp[n-1][amount];
         return(ans>=(int)1e9)? -1:ans;
-    }
-    public int solve(int i,int amount,int[] coins,Integer [][]dp){
-        if(amount==0) return 0;
-        if(i==0){
-            if(amount%coins[0]==0) return amount/coins[0];
-            else return (int)1e9;
-        }
-        if(dp[i][amount]!=null) return dp[i][amount];
-        int nottake = solve(i-1,amount,coins,dp);
-        int take = (int)1e9;
-        if(coins[i]<=amount){
-            take = 1 + solve(i,amount-coins[i],coins,dp);
-        }
-        return dp[i][amount]=Math.min(nottake,take);
     }
 }
