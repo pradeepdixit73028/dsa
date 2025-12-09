34a3555d8902ea756df916a348387992315bbb46
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-09 12:17:02 +0530

    Time: 7 ms (66.68%) | Memory: 46.2 MB (22.07%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        return count(nums,target,0,0);      
+    public int findTargetSumWays(int[] a, int target) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) sum += a[i];
+        Integer[][] dp = new Integer[a.length][(2*sum)+1];
+        return helper(a,a.length - 1,0,target,dp,sum);
     }
-    public int count(int nums[],int target,int sum,int i){
-        if(i==nums.length){
-            if(sum == target){
+    public static int helper(int[] a,int index,int sum,int target,Integer[][] dp,int total){
+        if(index < 0){
+            if(target == sum){
                 return 1;
+            }else{
+                return 0;
             }
-            return 0;
         }
-        int include = count(nums,target,sum+nums[i],i+1);
-        int exclude = count(nums,target,sum - nums[i],i+1);
-        return  (include + exclude);
+        int sumIndex = sum+total;
+        if(dp[index][sumIndex] != null){
+            if(dp[index][sumIndex] != 0)
+                return dp[index][sumIndex];
+            else return 0;
+        }
+        int plus = helper(a,index-1,sum + a[index],target,dp,total);
+        int minus = helper(a,index-1,sum - a[index],target,dp,total);
+        dp[index][sumIndex] = plus+minus;
+        return dp[index][sumIndex];
     }
 }
