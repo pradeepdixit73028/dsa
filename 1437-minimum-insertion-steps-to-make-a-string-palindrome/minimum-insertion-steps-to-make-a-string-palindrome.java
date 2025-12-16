610ec9e73bebe025ed249a721664405ecb94432d
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-16 12:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (96.02%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,39 @@
+class Solution {static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+                writer.write("0");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        int[] curr=new int[n+1];
+        int[] prev=new int[n+1];
+        Arrays.fill(prev,0);
+        for(int i=1;i<=m;i++){
+            curr[0]=0;
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    curr[j]=prev[j-1]+1;
+                }else{
+                    curr[j]=Math.max(curr[j-1],prev[j]);
+                }
+            }
+            for(int j=0;j<=n;j++){
+                prev[j]=curr[j];
+            }
+
+        }
+        return prev[n];
+    }
+    public int minInsertions(String s){
+        int n=s.length();
+        StringBuilder sb = new StringBuilder(s);
+        String t=sb.reverse().toString();
+        return n-longestCommonSubsequence(s,t);
+        
+    }
+}
