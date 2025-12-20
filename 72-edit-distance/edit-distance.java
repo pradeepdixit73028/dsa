b3e05341e0b95088881953d442f5e24c9736925b
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-20 18:30:46 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (8.88%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -11,20 +11,15 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m=word1.length();
         int n=word2.length();
-        Integer[][] dp= new Integer[m][n];
-        return solve(word1,word2, m-1,n-1,dp);
-    }
-    private int solve(String w1,String w2,int i,int j,Integer[][] dp){
-        if(i<0) return j+1; 
-        if(j<0) return i+1; 
-        if(dp[i][j]!=null) return dp[i][j];
-        if(w1.charAt(i)==w2.charAt(j)){
-            return dp[i][j]=solve(w1,w2,i-1,j-1,dp);
-        }else{
-            int insert = solve(w1,w2,i,j-1,dp);
-            int delete = solve(w1,w2,i-1,j,dp);
-            int replace = solve(w1,w2,i-1,j-1,dp);
-            return dp[i][j]=1+Math.min(replace,Math.min(insert, delete));
+        int[][] dp= new int[m+1][n+1];
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=0;j<=n;j++) dp[0][j]=j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j], dp[i][j-1]));
+            }
         }
+        return dp[m][n];
     }
 }
