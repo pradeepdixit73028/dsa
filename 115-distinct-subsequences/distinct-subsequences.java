b37c81fee1d6398da02986dda8e002d58785ece0
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-20 19:07:07 +0530

    Time: 19 ms (67.47%) | Memory: 54.3 MB (52.67%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,18 +1,20 @@
 class Solution {
-    private int recFunc(int i,int j,String s,String t,Integer[][] dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j]=recFunc(i-1,j-1,s,t,dp) + recFunc(i-1,j,s,t,dp);
-        }else{
-            return dp[i][j]=recFunc(i-1,j,s,t,dp);
-        } 
-    }
     public int numDistinct(String s, String t){
         int n = s.length();
         int m = t.length();
-        Integer[][] dp=new Integer[n][m];
-        return recFunc(n-1, m-1,s,t,dp); 
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+         for(int j =1;j<=m;j++){
+            dp[0][j] = 0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }
 }
