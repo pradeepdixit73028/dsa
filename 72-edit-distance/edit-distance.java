5ad89e98b8011c1af3b17773ca82f249f6cfbaac
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-20 18:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (75.09%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -9,17 +9,28 @@ class Solution {
         }));
     }
     public int minDistance(String word1, String word2) {
-        int m=word1.length();
-        int n=word2.length();
-        int[][] dp= new int[m+1][n+1];
-        for(int i=0;i<=m;i++) dp[i][0]=i;
-        for(int j=0;j<=n;j++) dp[0][j]=j;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j], dp[i][j-1]));
-            }
+        int n1=word1.length();
+        int n2=word2.length();
+        int[] prev=new int[n2+1];
+        int[] curr=new int[n2+1];
+        for(int j=0;j<=n2;j++){
+            prev[j]=j;
         }
-        return dp[m][n];
+        for(int i=1;i<=n1;i++){
+            curr[0]=i;
+            for(int j=1;j<=n2;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    curr[j]=prev[j-1];
+                }else{
+                    int in=1+curr[j-1];
+                    int d=1+prev[j];
+                    int r=1+prev[j-1];
+                    curr[j]=Math.min(in,Math.min(d,r));
+                }
+            }
+            prev=curr.clone();
+        }
+        return prev[n2];
+
     }
 }
