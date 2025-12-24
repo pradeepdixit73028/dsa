82279e334c3fc8da3b2b795b507649f97ba7cc01
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-24 11:46:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.19%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        int n=nums.length;
+        if(n<2) return n;
+        int prevdiff=nums[1]-nums[0];
+        int count=1;
+        if(prevdiff!=0) count=2;
+        for(int i=2;i<n;i++){
+            int diff=nums[i]-nums[i-1];
+            if((diff>0&&prevdiff<=0)||(diff<0&&prevdiff>=0)){
+                count++;
+                prevdiff=diff;
+
+            } 
+        }
+        return count;
+    } 
+}
