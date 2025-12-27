3a0600412cc011a7bb954e945baa2699fb2275cc
pradeepdixit73028 <mamtadixit11p@gmail.com>
2025-12-27 19:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (13.23%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+                writer.write("0");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+    public boolean isPerfectSquare(int num) {
+        long s=1,e=num/2+1,mid=s+(e-s)/2;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid>num) e=mid-1;
+            else s=mid+1;
+        }
+        return false;
+    }
+}
