1b5f79c5de335713e0d1e0275cdb056895057f69
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-01-05 15:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (31.50%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.java
+++ 233-number-of-digit-one/number-of-digit-one.java
@@ -1,15 +1,20 @@
 class Solution {
     public int countDigitOne(int n) {
-    if (n<=0) return 0;
-    int q=n, x=1, ans=0;
-    do {
-        int digit =q%10;
-        q/=10;
-        ans+=q*x;
-        if (digit==1) ans+=n%x+1;
-        if (digit>1) ans+=x;
-        x *=10;
-    } while(q>0);
-    return ans;
+        String a=String.valueOf(n);
+        Integer[][][] dp=new Integer[11][2][10];
+        return solve(a,0,1,0,dp);
     }
+    int solve(String s,int idx,int tight,int count,Integer[][][] dp){
+        if(idx==s.length()) return count;
+        if(dp[idx][tight][count]!=null) return dp[idx][tight][count];
+        int lb=0;
+        int ub=(tight==1) ? s.charAt(idx)-'0':9;
+        int res=0;
+        for(int digit=lb;digit<=ub;digit++){
+            int new_tight=(tight==1 && digit==ub) ? 1:0;
+            if(digit==1) res += solve(s,idx+1,new_tight,count+1,dp);
+            else res += solve(s,idx+1,new_tight,count,dp);
+        }
+        return dp[idx][tight][count]=res;
+    } 
 }
