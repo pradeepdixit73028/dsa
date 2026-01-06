88024dad6a0234482d8e9b2e7bbba89df9d266f7
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-01-06 09:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 74.2 MB (5.05%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
@@ -8,13 +8,16 @@ class Solution {
             }
         }));
     }
-    public int numDupDigitsAtMostN(int n) {
+    Integer[][][][][] dp;
+    static final int len=11;
+    static final int masklen=1024;
+    public int numDupDigitsAtMostN(int n){
         String a=String.valueOf(n);
-        Integer[][][][][] dp=new Integer[11][2][2][1024][2];
-        int ans=solve(a,0,1,0,0,1,dp);
+        dp=new Integer[len][2][2][masklen][2];
+        int ans=solve(a,0,1,0,0,1);
         return ans;
     }
-    public int solve(String s,int idx,int tight,int repeated,int mask,int lz,Integer[][][][][] dp){
+    public int solve(String s,int idx,int tight,int repeated,int mask,int lz){
         if(idx==s.length()) return repeated;
         if(dp[idx][tight][repeated][mask][lz]!=null) return dp[idx][tight][repeated][mask][lz];
         int lb=0;
@@ -22,12 +25,12 @@ class Solution {
         int res=0;
         for(int digit=lb;digit<=ub;digit++){
             int new_tight=(tight==1 && digit==ub) ? 1:0;
-            if(lz==1 && digit==0) res +=solve(s,idx+1,new_tight,repeated,mask,lz,dp);
+            if(lz==1 && digit==0) res +=solve(s,idx+1,new_tight,repeated,mask,lz);
             else{
                 int used=1 & (mask>>digit);
                 int new_repeated=(repeated==1||used==1) ? 1:0;
                 int new_mask=(1<<digit) | mask;
-                res += solve(s,idx+1,new_tight,new_repeated,new_mask,0,dp);
+                res += solve(s,idx+1,new_tight,new_repeated,new_mask,0);
             }
         }
         return dp[idx][tight][repeated][mask][lz]=res;
