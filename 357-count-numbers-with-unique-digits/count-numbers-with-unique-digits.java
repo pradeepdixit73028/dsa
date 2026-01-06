0b5fa540b1095d93ef2ecdaeae6e51ae7c0604c4
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-01-06 10:41:34 +0530

    Time: 12 ms (14.14%) | Memory: 47.6 MB (5.83%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,14 +1,35 @@
 class Solution{
     public int countNumbersWithUniqueDigits(int n){
-        if (n==0) {
-            return 1;
+        int x=(int)Math.pow(10,n);
+        String a=String.valueOf(x);
+        int ans=numDupDigitsAtMostN(x-1);
+        return x-ans;
+    }
+    Integer[][][][][] dp;
+    static final int len=11;
+    static final int masklen=1024;
+    public int numDupDigitsAtMostN(int n){
+        String a=String.valueOf(n);
+        dp=new Integer[len][2][2][masklen][2];
+        int ans=solve(a,0,1,0,0,1);
+        return ans;
+    }
+    public int solve(String s,int idx,int tight,int repeated,int mask,int lz){
+        if(idx==s.length()) return repeated;
+        if(dp[idx][tight][repeated][mask][lz]!=null) return dp[idx][tight][repeated][mask][lz];
+        int lb=0;
+        int ub=(tight==1) ? s.charAt(idx)-'0':9;
+        int res=0;
+        for(int digit=lb;digit<=ub;digit++){
+            int new_tight=(tight==1 && digit==ub) ? 1:0;
+            if(lz==1 && digit==0) res +=solve(s,idx+1,new_tight,repeated,mask,lz);
+            else{
+                int used=1 & (mask>>digit);
+                int new_repeated=(repeated==1||used==1) ? 1:0;
+                int new_mask=(1<<digit) | mask;
+                res += solve(s,idx+1,new_tight,new_repeated,new_mask,0);
+            }
         }
-        int total=10;
-        int prod=9;
-        for(int i=2;i<n+1;i++){
-            total+=prod*(11-i);
-            prod*=11-i;
-        }
-        return total;
+        return dp[idx][tight][repeated][mask][lz]=res;
     }
 }
