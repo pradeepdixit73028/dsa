533b6582d87721a9ed3e66abf5044be4aae91646
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-01-06 10:45:52 +0530

    Time: 155 ms (13.79%) | Memory: 69.8 MB (5.17%) - LeetSync

---
+++ 2457-count-special-integers/count-special-integers.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countSpecialNumbers(int n) {
+        return n-numDupDigitsAtMostN(n);
+    }
+    Integer[][][][][] dp;
+    static final int len=11;
+    static final int masklen=1024;
+    public int numDupDigitsAtMostN(int n){
+        String a=String.valueOf(n);
+        dp=new Integer[len][2][2][masklen][2];
+        int ans=solve(a,0,1,0,0,1);
+        return ans;
+    }
+    public int solve(String s,int idx,int tight,int repeated,int mask,int lz){
+        if(idx==s.length()) return repeated;
+        if(dp[idx][tight][repeated][mask][lz]!=null) return dp[idx][tight][repeated][mask][lz];
+        int lb=0;
+        int ub=(tight==1) ? s.charAt(idx)-'0':9;
+        int res=0;
+        for(int digit=lb;digit<=ub;digit++){
+            int new_tight=(tight==1 && digit==ub) ? 1:0;
+            if(lz==1 && digit==0) res +=solve(s,idx+1,new_tight,repeated,mask,lz);
+            else{
+                int used=1 & (mask>>digit);
+                int new_repeated=(repeated==1||used==1) ? 1:0;
+                int new_mask=(1<<digit) | mask;
+                res += solve(s,idx+1,new_tight,new_repeated,new_mask,0);
+            }
+        }
+        return dp[idx][tight][repeated][mask][lz]=res;
+    }
+}
