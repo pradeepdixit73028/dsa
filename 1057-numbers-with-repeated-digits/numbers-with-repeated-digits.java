884d271063ec59afb60c8ce3718ef44e9d541ea9
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-01-06 09:36:40 +0530

    Time: 185 ms (7.57%) | Memory: 55.2 MB (5.55%) - LeetSync

---
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDupDigitsAtMostN(int n) {
+        String a=String.valueOf(n);
+        Integer[][][][][] dp=new Integer[11][2][2][1024][2];
+        int ans=solve(a,0,1,0,0,1,dp);
+        return ans;
+    }
+    public int solve(String s,int idx,int tight,int repeated,int mask,int lz,Integer[][][][][] dp){
+        if(idx==s.length()) return repeated;
+        if(dp[idx][tight][repeated][mask][lz]!=null) return dp[idx][tight][repeated][mask][lz];
+        int lb=0;
+        int ub=(tight==1) ? s.charAt(idx)-'0':9;
+        int res=0;
+        for(int digit=lb;digit<=ub;digit++){
+            int new_tight=(tight==1 && digit==ub) ? 1:0;
+            if(lz==1 && digit==0) res +=solve(s,idx+1,new_tight,repeated,mask,lz,dp);
+            else{
+                int used=1 & (mask>>digit);
+                int new_repeated=(repeated==1||used==1) ? 1:0;
+                int new_mask=(1<<digit) | mask;
+                res += solve(s,idx+1,new_tight,new_repeated,new_mask,0,dp);
+            }
+        }
+        return dp[idx][tight][repeated][mask][lz]=res;
+    }
+}
