255001accedcd758617f78aee96fa23390611dd0
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-02-03 14:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 110.7 MB (84.97%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,16 +1,8 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int n = edges.length+1;
-        int[] degree=new int[n+1];
-        for(int[] edge:edges){
-            degree[edge[0]]++;
-            degree[edge[1]]++;
-        }
-        for(int i=1;i<=n;i++){
-            if(degree[i]==n-1){
-                return i;
-            }
-        }
-        return -1;
+        if(edges[0][0]==edges[1][0]||edges[0][0]==edges[1][1])
+            return edges[0][0];
+        else
+            return edges[0][1];
     }
 }
