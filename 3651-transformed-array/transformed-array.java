0108737a2208bc64678b5fe2998f8c49f717abf5
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-02-05 13:43:36 +0530

    Time: 1 ms (98.72%) | Memory: 46.3 MB (96.47%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -3,17 +3,7 @@ class Solution {
         int n=nums.length;
         int[] result=new int[n];
         for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                int temp=0;
-                temp=n-Math.abs(nums[i])%n+i;
-                result[i]=nums[temp%n];
-            }
-            else if(nums[i]==0){
-                result[i]=nums[i];
-            }
-            else{
-                result[i]=nums[(nums[i]%n+i)%n];
-            }
+            result[i] = nums[((i + nums[i]) % n + n) % n];
         }
         return result;
     }
