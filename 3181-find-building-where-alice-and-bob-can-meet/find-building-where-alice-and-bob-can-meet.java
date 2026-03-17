c27e799459d064ebf1b24f021411a973f403b8a2
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-03-17 12:06:24 +0530

    Time: 535 ms (38.28%) | Memory: 169.4 MB (75.00%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.java
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.java
@@ -1,33 +1,24 @@
 class Solution {
-    static int[] tree;
-    static int[] arr;
-    static int n;
     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        n=heights.length;
-        arr=heights;
+        int n=heights.length;
         int q=queries.length;
-        tree=new int[4*n];
+        int[] tree=new int[4*n];
         int[] ans=new int[q];
-        buildTree(0,0,n-1);
+        buildTree(0,0,n-1,heights,tree);
         for(int i=0;i<q;i++){
-            int a=queries[i][0];
-            int b=queries[i][1];
-            if(a>b){
-                int temp=a;
-                a=b;
-                b=temp;
-            }
-            if(a==b) ans[i]=a;
-            else if(heights[a]<heights[b]) ans[i]=b;
+            int alice=Math.min(queries[i][0],queries[i][1]);
+            int bob=Math.max(queries[i][0],queries[i][1]);
+            if(alice==bob) ans[i]=alice;
+            else if(heights[alice]<heights[bob]) ans[i]=bob;
             else{
-                ans[i]=solve(0,0,n-1,b+1,n-1,heights[a]);
+                ans[i]=solve(0,0,n-1,bob+1,n-1,heights[alice],tree);
             }
             
         }
         return ans;
         
     }
-    public static void buildTree(int idx,int start,int end){
+    public static void buildTree(int idx,int start,int end,int[] arr,int[] tree){
         if(start==end){
             tree[idx]=arr[start];
             return;
@@ -35,11 +26,11 @@ class Solution {
         int mid=start+(end-start)/2;
         int left=2*idx+1;
         int right=2*idx+2;
-        buildTree(left,start,mid);//left
-        buildTree(right,mid+1,end);//right
+        buildTree(left,start,mid,arr,tree);//left
+        buildTree(right,mid+1,end,arr,tree);//right
         tree[idx]=Math.max(tree[left],tree[right]);//root sum
     }
-    public int solve(int idx,int left,int right,int qs,int qe,int target){
+    public int solve(int idx,int left,int right,int qs,int qe,int target,int[] tree){
         if(right<qs || left>qe) return -1;
         if(tree[idx]<=target) return -1;
         if(left==right){
@@ -48,8 +39,8 @@ class Solution {
         int mid=left+(right-left)/2;
         int leftchild=2*idx+1;
         int rightchild=2*idx+2;
-        int ans1=solve(leftchild,left,mid,qs,qe,target);
-        int ans2=solve(rightchild,mid+1,right,qs,qe,target);
+        int ans1=solve(leftchild,left,mid,qs,qe,target,tree);
+        int ans2=solve(rightchild,mid+1,right,qs,qe,target,tree);
         if(ans1!=-1) return ans1;
         else return ans2;
     }
