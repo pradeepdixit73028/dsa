c70a1fb67fd8b56aef71efd00c92a0a561951f59
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-04-16 09:49:08 +0530

    Time: 4 ms (13.77%) | Memory: 45.9 MB (37.84%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -4,17 +4,17 @@ class Solution {
     Arrays.sort(nums);
     backtrack(l, new ArrayList<>(), nums, target, 0);
     return l;
-}
+    }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tl, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tl));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tl.add(nums[i]);
-            backtrack(list, tl, nums, remain - nums[i], i);
-            tl.remove(tl.size() - 1);
+    private void backtrack(List<List<Integer>> list, List<Integer> tl, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tl));
+        else{ 
+            for(int i = start; i < nums.length; i++){
+                tl.add(nums[i]);
+                backtrack(list, tl, nums, remain - nums[i], i);
+                tl.remove(tl.size() - 1);
+            }
         }
     }
-}
 }
