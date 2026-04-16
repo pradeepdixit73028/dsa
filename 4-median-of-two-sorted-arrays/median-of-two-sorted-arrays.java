4acd337a82dce8d169e918903266302c3e78eaba
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-04-16 09:30:36 +0530

    Time: 1 ms (100.00%) | Memory: 48.4 MB (98.14%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,19 +1,23 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length>nums2.length) return findMedianSortedArrays(nums2,nums1);
         int m=nums1.length;
         int n=nums2.length;
-        int[] merged=new int[m+n];
-        int i=0,j=0,k=0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]) merged[k++]=nums1[i++];
-            else merged[k++]=nums2[j++];
+        int l=0,r=m;
+        while(l<=r){
+            int px=l+(r-l)/2;
+            int py=(m+n+1)/2-px;
+            int x1=(px==0) ? Integer.MIN_VALUE:nums1[px-1];
+            int x2=(py==0) ? Integer.MIN_VALUE:nums2[py-1];
+            int x3=(px==m) ? Integer.MAX_VALUE:nums1[px];
+            int x4=(py==n) ? Integer.MAX_VALUE:nums2[py];
+            if(x1<=x4 && x2<=x3){
+                if((m+n)%2==1) return Math.max(x1,x2);
+                else return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
+            }
+            if(x1>x4) r=px-1;
+            else l=px+1;
         }
-        while(i<m) merged[k++]=nums1[i++];
-        while(j<n) merged[k++]=nums2[j++];
-
-        int p=merged.length;
-
-        if(p%2==1) return merged[p/2];
-        else return (merged[p/2] + merged[p/2-1])/2.0;
+        return 0;
     }
 }
