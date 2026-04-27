599f2810c57bbc242f12bee7e5c1dbb58845e32b
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-04-27 12:08:17 +0530

    Time: 1 ms (99.97%) | Memory: 77.2 MB (66.84%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,25 +1,16 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
-    }
-    public int maxSubArray(int[] nums) {
-        int max_sum = Integer.MIN_VALUE;
-        int curr_sum =0;
+    public int maxSubArray(int[] nums){
+        int max=Integer.MIN_VALUE;
+        int curr=0;
         for(int i=0;i<nums.length;i++){
-            curr_sum += nums[i];
-            if(curr_sum>max_sum){
-                max_sum=curr_sum;
+            curr+=nums[i];
+            if(curr>max){
+                max=curr;
             }
-            if(curr_sum<0){
-                curr_sum=0;
+            if(curr<0){
+                curr=0;
             }
         }
-        return max_sum;
+        return max;
     }
 }
