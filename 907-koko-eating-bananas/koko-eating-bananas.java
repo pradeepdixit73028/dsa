edfb08dbfe65266a4d4531fc6e18b6235afd2a53
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-04-29 10:33:07 +0530

    Time: 7 ms (80.45%) | Memory: 48.2 MB (18.17%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,32 +1,30 @@
 class Solution {
-    public boolean isPossible(int mid, int[] arr, int h){
-        long hours = 0;
+    public int minEatingSpeed(int[] arr,int h) {
         int n = arr.length;
+        int low=1;
+        int high=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            hours += (arr[i] + mid - 1) / mid; 
+            high=Math.max(high,arr[i]);
         }
-        if(hours>h) return false;
-        return true;
-    }
-    public int minEatingSpeed(int[] arr, int h) {
-        int n = arr.length;
-        int lo = 1;
-        int hi = Integer.MIN_VALUE;
-        
-        for(int i=0;i<n;i++){
-            hi = Math.max(hi,arr[i]);
-        }
-        int ans = hi;
-        while(lo<=hi){
-            int mid = lo +(hi-lo)/2;
-            if(isPossible(mid,arr,h)){
-                ans = mid;
-                hi = mid-1;
-            }
-            else{
-                lo = mid+1;
+
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(arr,mid,h)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return ans;
+    }
+    public boolean isPossible(int[] arr,int mid,int h){
+        long time=0;
+        for(int i=0;i<arr.length;i++){
+            time+=(arr[i]+mid-1)/mid;
+        }
+        if(time>h) return false;
+        return true;
     }
 }
