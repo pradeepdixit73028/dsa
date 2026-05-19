ac215db3b330bb6cf52cee46f789442338ca96a5
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-05-19 14:59:55 +0530

    Time: 407 ms (25.17%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-
+with cte as(
+    select
+        email as email,
+        COUNT(email) as count
+    from
+        Person
+    group by (email)
+)
 select email
-from person
-group by email
-having count(email)>1;
+from cte
+where count>1
