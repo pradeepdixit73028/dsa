4e5cb298b309ceafddbade8668c9653923472978
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-05-26 14:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (64.44%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toHex(int num) {
+        if(num==0) return "0";
+        String s="0123456789abcdef";
+        char[] hexChars = s.toCharArray(); 
+        StringBuilder sb=new StringBuilder();
+        while(num!=0){
+            int rem=num & 15;
+            sb.append(hexChars[rem]);
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+}
