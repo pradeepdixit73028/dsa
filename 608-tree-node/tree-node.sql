146d0cdb593bb9dbaa6470580c101918ed809def
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-06-02 14:26:50 +0530

    Time: 516 ms (28.78%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,16 +1,23 @@
 # Write your MySQL query statement below
-
-SELECT t.id,
+WITH RECURSIVE Hierarchy AS(
+    SELECT 
+        id, p_id
+    FROM Tree
+    WHERE p_id IS NULL
+    UNION ALL
+    SELECT 
+        t.id,t.p_id
+    FROM Tree t
+    INNER JOIN Hierarchy h ON t.p_id=h.id
+)
+SELECT 
+    h.id,
     CASE 
-        WHEN t.p_id IS NULL 
-         THEN 'Root'
-         WHEN t.id NOT IN 
-         (SELECT DISTINCT p_id 
-            FROM Tree 
-            WHERE p_id IS NOT NULL) 
-         THEN 'Leaf'
+        WHEN h.p_id IS NULL THEN 'Root'
+        WHEN c.p_id IS NULL THEN 'Leaf'
         ELSE 'Inner'
     END AS type
-FROM Tree t;
+FROM Hierarchy h
+LEFT JOIN (SELECT DISTINCT p_id FROM Tree)c ON h.id=c.p_id;
 
 		
