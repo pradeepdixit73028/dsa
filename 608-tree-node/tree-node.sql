4562d2382aafa04431bd083a00f713f645cc4a13
pradeepdixit73028 <mamtadixit11p@gmail.com>
2026-06-02 14:06:57 +0530

    Time: 461 ms (82.77%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,15 +1,5 @@
 # Write your MySQL query statement below
-WITH RECURSIVE Hierarchy AS(
-    SELECT 
-        id, p_id,1 AS depth
-    FROM Tree
-    WHERE p_id IS NULL
-    UNION ALL
-    SELECT 
-        t.id,t.p_id,h.depth+1 AS depth
-    FROM Tree t
-    INNER JOIN Hierarchy h ON t.p_id=h.id
-)
+
 SELECT t.id,
     CASE 
         WHEN t.p_id IS NULL 
